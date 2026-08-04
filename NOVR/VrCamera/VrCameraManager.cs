3568e47838ed96ad5401c6e46e542c3ae1bda66b
Mark Foley <mark@earlytrade.com>
2026-08-04 16:37:07 +1000

    Render VR zoom through URP XR passes

--- NOVR/VrCamera/VrCameraManager.cs
+++ NOVR/VrCamera/VrCameraManager.cs
@@ -107,7 +107,6 @@ public class VrCameraManager: MonoBehaviour
         ReparentTrackedChildren(rootCamera.transform, trackedCameraObject.transform);
 
         trackedCameraObject.AddComponent<VrCamera>();
-        trackedCameraObject.AddComponent<VrZoomController>();
 
         IgnoredCameras.Add(rootCamera);
         IgnoredCameras.Add(trackedCamera);
@@ -140,11 +139,6 @@ public class VrCameraManager: MonoBehaviour
         if (trackedCamera.GetComponent<VrCamera>() == null)
         {
             trackedCamera.gameObject.AddComponent<VrCamera>();
-        }
-
-        if (trackedCamera.GetComponent<VrZoomController>() == null)
-        {
-            trackedCamera.gameObject.AddComponent<VrZoomController>();
         }
 
         IgnoredCameras.Add(rootCamera);
